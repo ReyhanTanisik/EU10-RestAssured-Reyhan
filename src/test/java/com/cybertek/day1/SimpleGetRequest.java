d2a5b72ae922116b18711e6abdac1df0739ed224
Reyhan <113282262+ReyhanTanisik@users.noreply.github.com>
2023-03-29 12:25:17 +0300

    Initial commit

---
+++ src/test/java/com/cybertek/day1/SimpleGetRequest.java
@@ -0,0 +1,25 @@
+package com.cybertek.day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url="http://54.152.217.128:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        // send a request and save response inside the response object
+      Response response=  RestAssured.get(url);
+        System.out.println(response.statusCode());
+
+        // print response body
+        response.prettyPrint();
+
+    }
+
+
+
+}
