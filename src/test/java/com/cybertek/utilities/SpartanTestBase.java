67c0e60382762cd8fcb9cd9766952ad53d9b08cf
Reyhan <113282262+ReyhanTanisik@users.noreply.github.com>
2023-04-03 11:38:33 +0300

    spartan test base

---
+++ src/test/java/com/cybertek/utilities/SpartanTestBase.java
@@ -0,0 +1,15 @@
+package com.cybertek.utilities;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class SpartanTestBase {
+
+    @BeforeAll
+    public static void init() {
+
+        // save base url inside this variable so that we dont need to
+        //type for each test
+        RestAssured.baseURI = "http://54.152.217.128:8000";
+    }
+}
