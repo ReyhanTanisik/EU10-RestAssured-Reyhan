4e7bcf1759aba5e1e7147c70f137891b74c056b1
Reyhan <113282262+ReyhanTanisik@users.noreply.github.com>
2023-04-06 13:18:34 +0300

    json to java test

---
+++ src/test/java/com/cybertek/day5/JsonToJavaTest.java
@@ -0,0 +1,54 @@
+package com.cybertek.day5;
+
+
+import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import  static io.restassured.RestAssured.*;
+
+public class JsonToJavaTest extends SpartanTestBase {
+
+@DisplayName("get one spartan and deserialize to map")
+    @Test
+    public void oneSpartanToMap(){
+
+   Response response= given().pathParam("id",15)
+            .when().get("/api/spartans/{id}").then()
+            .statusCode(200).extract().response();
+
+
+   // get the json and convert it to map
+
+    Map<String,Object> jsonMap=response.as(Map.class);
+    System.out.println(jsonMap.toString());
+
+    String actualName=(String) jsonMap.get("name");
+    assertThat(actualName,is("Meta"));
+
+
+}
+
+@DisplayName("get all spartans to java data structure")
+    @Test
+    public void getAllSpartan(){
+
+    Response response=get("/api/spartans").then().statusCode(200)
+            .extract().response();
+
+    // we need to convert json to java which is deserialize
+
+    List<Map<String,Object>> jsonList=response.as(List.class);
+    System.out.println("jsonList.get(1).get(\"name\") = " + jsonList.get(1).get("name"));
+    Map<String,Object> spartan3=jsonList.get(2);
+    System.out.println(spartan3);
+
+}
+
+}
