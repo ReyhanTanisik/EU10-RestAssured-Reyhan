2e36824f2fa28751c68fd581678c1a69f6bf456c
Reyhan <113282262+ReyhanTanisik@users.noreply.github.com>
2023-04-06 13:19:21 +0300

    spartan test base

--- src/test/java/com/cybertek/utilities/SpartanTestBase.java
+++ src/test/java/com/cybertek/utilities/SpartanTestBase.java
@@ -1,6 +1,7 @@
 package com.cybertek.utilities;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public class SpartanTestBase {
@@ -11,5 +12,19 @@ public class SpartanTestBase {
         // save base url inside this variable so that we dont need to
         //type for each test
         RestAssured.baseURI = "http://54.152.217.128:8000";
+
+
+
+        String dbUrl="jdbc:oracle:thin:@54.152.217.128:1521:XE";
+        String dbUsername="SP";
+        String dbPassword="SP";
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+
+    }
+
+    @AfterAll
+    public static void teardown(){
+        DBUtils.destroy();
     }
 }
