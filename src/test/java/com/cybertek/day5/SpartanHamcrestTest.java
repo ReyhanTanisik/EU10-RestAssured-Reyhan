8d5a67f41844f84c0fcab1a55ccf6fe389573993
Reyhan <113282262+ReyhanTanisik@users.noreply.github.com>
2023-04-06 13:19:09 +0300

    spartan hamcrest test

--- src/test/java/com/cybertek/day5/SpartanHamcrestTest.java
+++ src/test/java/com/cybertek/day5/SpartanHamcrestTest.java
@@ -1,7 +1,10 @@
 package com.cybertek.day5;
 import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -11,6 +14,15 @@ public class SpartanHamcrestTest extends SpartanTestBase {
     @DisplayName("Get spartan/search and chaining together")
     @Test
     public void test1(){
+    List<String> names= given().accept(ContentType.JSON)
+                .and().queryParams("nameContains","j",
+                        "gender","male")
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .and().body("totalElement",is(3))
+                .extract().response().jsonPath().getList("content.name");
+
+        System.out.println(names);
 
 
 
